8bf6981e87deab207f842d3b5b5c28ba282134fc
Cameron Ferguson <cameron.w.ferguson@gmail.com>
2015-02-26 13:07:48 -0800

    Script now appends to existing file, or makes new one. Plus dates added to sheet names

--- failurelist.R
+++ failurelist.R
@@ -4,14 +4,10 @@
 # calculate percent of each reason
 library(dplyr)
 library(xlsx)
-filename <- readline("What is the file called? 
-                     Don't forget the extension... ")
+filename <- readline("What is the file called? Don't forget the extension... ")
 
-outputname <- readline("What do you want the output to be called?... ")
+outputname <- readline("What is the output file called?...  ")
 outputname <- paste(outputname, ".xlsx", sep="")
-
-raw_or_count <- readline("Do you want a counted list or a raw list? 
-                         1 for counted, 2 for raw... ")
 
 rawdata <- read.table(filename, 
                       sep="\t", 
@@ -32,16 +28,23 @@ sorted <- arrange(failures, Failure_Reason, Five_Prime_mod, Three_Prime_mod)
 by_Reason <- group_by(sorted, Failure_Reason)
 failure_counts <- summarize(by_Reason, number_of_failures = n())
 class(failure_counts) <- "data.frame"
+date <- Sys.Date()
+
+
+countedname <- paste("Counted", "for", date, sep=" ")
+rawname <- paste("Raw", "for", date, sep=" ")
 
 write.xlsx(failure_counts,
            file=outputname, 
-           sheetName="Counted",
-           row.names=FALSE)
+           sheetName=countedname,
+           row.names=FALSE,
+           append=TRUE)
 
 write.xlsx(sorted, 
            outputname,
-           sheetName="Raw",
-           row.names=FALSE)
+           sheetName=rawname,
+           row.names=FALSE,
+           append=TRUE)
 
 
 
